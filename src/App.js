9f12b1a4aa09b1d05473b5737cf7ca934a8fa26d
mohammadzakout1 <mzakout@madrewards.com>
2023-05-14 23:26:19 +0300

    add new message

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, {useState, useEffect} from 'react';
 const { io } = require("socket.io-client");
 
 let socket = null;
+const messageList = [];
 function App() {
 
   const [count, setCount] = useState(0); 
@@ -11,6 +12,23 @@ function App() {
     console.log('rendering...');
   // after component mount...
   useEffect(() => {
+
+
+
+
+for (var i = 0; i < messageList.length; i++) {
+  console.log(messageList[i]);
+  var list = document.createElement('li');
+  list.setAttribute("id", "myList");
+  var item = document.createElement('li');
+  item.appendChild(document.createTextNode(messageList[i].body));
+  list.appendChild(item);
+  document.body.appendChild(list);
+
+
+}
+
+
 
   console.log('component mounted');
     // connect to the server
@@ -27,6 +45,7 @@ function App() {
 
       socket.on('connect', function() {
         console.log('connected to server');
+        
       }
       );
  
@@ -44,6 +63,7 @@ var list = document.createElement('ul');
 list.setAttribute("id", "myList");
 
 for (var i = 0; i < obj.length; i++) {
+  console.log(obj[i]);
 
   var item = document.createElement('li');
   item.appendChild(document.createTextNode(obj[i].full_name));
@@ -52,6 +72,18 @@ for (var i = 0; i < obj.length; i++) {
   
   list.appendChild(item);
   item.appendChild(lastMessage);
+  var br = document.createElement("br");
+  item.appendChild(br);
+  var buttin = document.createElement("button");
+  buttin.textContent = "get Message";
+  item.appendChild(buttin);
+  buttin.addEventListener ("click", function() {
+   
+    socket.emit('openConversation' , {"conversationid" : 58});
+
+
+
+  });
 
   
 }
@@ -61,15 +93,44 @@ document.body.appendChild(list);
       // set the new count on the client
     
     })
+
+socket.on('getConversationMessages', function(message) {
+  console.log('received updated count from server: ' + message);
+  const obj = JSON.parse(JSON.stringify(message));
+  messageList.push(obj);
+console.log(obj);
+
+
+})
+socket.on('newMessage', function(message) {
+  console.log('received new message: ' + message);
+  const obj = JSON.parse(JSON.stringify(message));
+  messageList.push(obj);
+console.log(obj);
+});
+
+
+
    
     
   }, []);
   return (
-    <button onClick={handleClick}>Request Messages of Technical support: {count}</button>
+    <div>
+
+
+    <button onClick={handleClick}>Request Messages of Technical support: {count}</button><br/>
+    <h1>get Message With naeem</h1>
+    <button onClick={handleClick2}>get Message With naeem {count}</button>
+    </div>
   );
 }
 
 function handleClick() {
+  // we emit this event that increments the count on the server
+  // and the updated count is emitted back via 'counter updated'
+  socket.emit('requestMessageList' , {"type" : 2 , key : ""});
+}
+function handleClick2() {
   // we emit this event that increments the count on the server
   // and the updated count is emitted back via 'counter updated'
   socket.emit('requestMessageList' , {"type" : 2 , key : ""});
